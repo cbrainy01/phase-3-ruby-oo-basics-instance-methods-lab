b46e66fc351c1e9c4b051cfe12e3044c527d2478
Carl <carlbrainegebe@gmail.com>
2021-07-19 18:26:33 -0500

    commit tree 61400938f8a4fdbdb54e3b72bdfd07392746ff0d

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+# create instance of dog for snoopy
+snoopy = Dog.new
+snoopy.bark
